db811fcdbaf6f1a8d1b03fc34ee5e2fd56679494
lethanh <thanhbok26b@gmail.com>
2017-10-30 10:50:17 +0800

    x

--- model.py
+++ model.py
@@ -19,15 +19,20 @@ def create_graph(hyper_params=hyper_params):
         with tf.name_scope('input') as scope:
             X = tf.placeholder(tf.float32, [None, hyper_params['num_features']]) 
             Y = tf.placeholder(tf.int32, [None])
+            X_test = tf.placeholder(tf.float32, [None, hyper_params['num_features']]) 
+            Y_test = tf.placeholder(tf.int32, [None])
 
         with tf.name_scope('fc') as scope:
             i = 0
             out = X
+            out_test = X_test
+
             last_hidden = hyper_params['num_features']
             for num_hidden in hyper_params['num_hiddens_fc']:
                 W = tf.Variable(tf.truncated_normal([last_hidden, num_hidden],stddev=0.1, name='W_%d' % i))
                 b = tf.Variable(tf.constant(0., shape=[num_hidden], name='b_%d' % i))
                 out = tf.nn.relu(tf.nn.xw_plus_b(out, W, b))
+                out_test = tf.nn.relu(tf.nn.xw_plus_b(out_test, W, b))
                 i += 1                
                 last_hidden = num_hidden
             i += 1
@@ -35,15 +40,19 @@ def create_graph(hyper_params=hyper_params):
             W = tf.Variable(tf.truncated_normal([last_hidden, num_output],stddev=0.1, name='W_%d' % i))
             b = tf.Variable(tf.constant(0., shape=[num_output], name='b_%d' % i))
             logits = tf.nn.xw_plus_b(out, W, b)
+            logits_test = tf.nn.xw_plus_b(out_test, W, b)
 
         with tf.name_scope('fc_loss_function') as scope:
             cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=Y, logits=logits, name='xentropy')
-            cost = tf.reduce_mean(cross_entropy, name='xentropy_mean')
+            cross_entropy_test = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=Y_test, logits=logits_test, name='xentropy_test')
+            cost = tf.reduce_mean(cross_entropy, name='xentropy_mean_test')
+            cost_test = tf.reduce_mean(cross_entropy_test, name='xentropy_mean_test')
 
         optimizer = tf.train.AdamOptimizer(hyper_params['learning_rate']).minimize(cost, global_step=global_step)
 
         with tf.name_scope('loss_by_step') as scope:
-            tf.summary.scalar('cost', cost)
+            tf.summary.scalar('cost_train', cost)
+            tf.summary.scalar('cost_test', cost_test)
             summary_op = tf.summary.merge_all()
 
     model_vars = {
